9dd1b432b6534c998751ae957f0b299ad9895ee9
Dan Brooks <dmbrooks@live.co.uk>
2017-04-08 12:38:23 +0100

    Use implicitly to get List / Option functors

--- src/test/scala/fsis/FunctorSpec.scala
+++ src/test/scala/fsis/FunctorSpec.scala
@@ -6,13 +6,13 @@ class FunctorSpec extends FreeSpec with Matchers {
 
   "listFunctor" - {
     "provides map for lists" in {
-      Functor.listFunctor.map(List(1, 2, 3))(_ + 10) should be(List(11, 12, 13))
+      implicitly[Functor[List]].map(List(1, 2, 3))(_ + 10) should be(List(11, 12, 13))
     }
   }
 
   "optionFunctor" - {
     "provides map for options" in {
-      Functor.optionFunctor.map(Some(3))(_ + 10) should be(Some(13))
+      implicitly[Functor[Option]].map(Some(3))(_ + 10) should be(Some(13))
     }
   }
 }
