danbroooks/fpinscala

Initial commit of project structure

build.sbt
@@ -1,0 +1,6 @@
+name := "fpinscala"
+
+scalaVersion := "2.11.7"
+
+libraryDependencies += "org.scalactic" %% "scalactic" % "3.0.0"
+libraryDependencies += "org.scalatest" %% "scalatest" % "3.0.0" % "test"

src/main/scala/fpinscala/Main.scala
@@ -1,0 +1,5 @@
+package fpinscala
+
+object Main extends App {
+  println("Run the tests with sbt test")
+}

danbroooks/fsis

build.sbt
@@ -1,0 +1,6 @@
+name := "fsis"
+
+scalaVersion := "2.11.7"
+
+libraryDependencies += "org.scalactic" %% "scalactic" % "3.0.0"
+libraryDependencies += "org.scalatest" %% "scalatest" % "3.0.0" % "test"

src/main/scala/fsis/Main.scala
@@ -1,0 +1,5 @@
+package fsis
+
+object Main extends App {
+  println("Run the tests with sbt test")
+}