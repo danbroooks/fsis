8706972afa084f0b0c3e0751816d2879f9aeaa09
Dan Brooks <dmbrooks@live.co.uk>
2018-04-03 12:00:29 +0100

    applicative

---
+++ src/main/scala/fsis/Applicative.scala
@@ -0,0 +1,39 @@
+package fsis
+
+import simulacrum._
+
+@typeclass trait Applicative[F[_]] extends Functor[F] {
+
+  def pure[A](a: A): F[A]
+
+  def apply[A, B](fa: F[A])(ff: F[A => B]): F[B]
+
+  override def map[A, B](fa: F[A])(f: A => B): F[B] =
+    apply(fa)(pure(f))
+
+  def map2[A, B, Z](fa: F[A], fb: F[B])(f: (A, B) => Z): F[Z] =
+    apply(fa)((map(fb)(b => f(_, b))))
+}
+
+object Applicative {
+
+  implicit val optionApplicative: Applicative[Option] = new Applicative[Option] {
+    def pure[A](a: A): Option[A] = Some(a)
+
+    def apply[A, B](fa: Option[A])(ff: Option[A => B]): Option[B] = (fa, ff) match {
+      case (None, _) => None
+      case (Some(a), None) => None
+      case (Some(a), Some(f)) => Some(f(a))
+    }
+  }
+
+  implicit val listApplicative: Applicative[List] = new Applicative[List] {
+    def pure[A](a: A): List[A] = List(a)
+
+    def apply[A, B](fa: List[A])(ff: List[A => B]): List[B] = for {
+      a <- fa
+      f <- ff
+    } yield f(a)
+  }
+}
+
